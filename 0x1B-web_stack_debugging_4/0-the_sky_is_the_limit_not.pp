72e377c50233eb97df897e2a5f3c19d5c0059c2e
gefaustina <famoust4luv@yahoo.com>
2023-05-18 21:11:35 -0700

    Limmit increased

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# This manuscript increases the amount of traffic an Nginx server can handle
+
+exec { 'ngix-conf':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
